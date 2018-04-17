2641bd1c41f7cf98fda95ffb2a2dd8f97f1124ee
truptigaonkar <trups11.trupti@gmail.com>
2018-04-17 13:52:13 +0200

    Each company can view their jobposts which are created

--- company/dashboard.php
+++ company/dashboard.php
@@ -33,14 +33,56 @@ if (isset($_SESSION['jobpostSuccess'])) {
   unset($_SESSION['jobpostSuccess']);
 }
 ?>
-
-
     <div class="row">
       <div class="col-md-12">
         <div class="card card-info">
           <div class="card-header"><h4>Job Post <small>Add, View, Edit, Delete</small><a href="createjobpost.php" class="btn btn-primary btn-block btn-xs col-md-2 pull-right">Add Jobpost</a></h4>       
           </div>
             <div class="card-body">
+            <table class="table table-striped table-bordered table-hover">
+                <thead> 
+                <tr> 
+                  <th>Job Title</th>
+                  <th>Job Description</th>
+                  <th>Job Salary</th>
+                  <th>Job Experience</th>
+                  <th>Job Qualification</th>
+                  <th>Job Created At</th>           
+                  <th>Action</th>
+                  </tr>
+                </thead>                                 
+                <tbody>
+                  <?php 
+                  $sql = "SELECT * FROM jobposts WHERE id_company='$_SESSION[id_company]' ";
+                  $result = $conn->query($sql);
+
+                //If Job Post exists then display details of post
+                  if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                      ?> 
+                      <tr> 
+                  <td><?php echo $row["jobtitle"]; ?></td>
+                  <td><?php echo $row["jobdescription"]; ?></td>
+                  <td><?php echo $row["jobsalary"]; ?></td>
+                  <td><?php echo $row["jobexperience"]; ?></td>
+                  <td><?php echo $row["jobqualification"]; ?></td>
+                  <td><?php echo $row["jobcreatedat"]; ?></td>
+                  <td>    
+                    <a href="student_delete.php?id=<?php echo $row["id_jobpost"]; ?>" onclick="return confirm('Are you sure to delete !'); " class="btn btn-primary" >Edit</a>
+                    <a href="student_delete.php?id=<?php echo $row["id_jobpost"]; ?>" onclick="return confirm('Are you sure to delete !'); " class="btn btn-danger" >Delete</a>  
+                  </td>
+                  </tr>
+                
+                  
+                <?php
+
+              }
+            }
+            $conn->close();
+            ?>
+
+                </tbody>                                                     
+              </table> 
             </div>
           </div>
         </div>
